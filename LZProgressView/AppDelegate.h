6a4f06f1ca2c484a1a472500374135ffb33dae19
mac <mac@clover.local>
2015-10-27 16:19:40 +0800

    Initial Commit

---
+++ LZProgressView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZProgressView
+//
+//  Created by mac on 10/27/15.
+//  Copyright © 2015 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LZProgressView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZProgressView
+//
+//  Created by mac on 10/27/15.
+//  Copyright © 2015 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
